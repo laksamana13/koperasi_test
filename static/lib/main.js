5ea643e2fba5ae04814a5af2142cef5ec4fc8d90
muhammadkevin <kodinginde12@gmail.com>
2020-11-29 22:59:41 +0700

    anggota panel

--- static/lib/main.js
+++ static/lib/main.js
@@ -2,64 +2,59 @@
 
 
 
-
-
-
-// DEFENITION SELECTOR
-//-----tambahAnggota---------- 
-const subFormShowHideBt = $('.showsubform');
-
-
-
-
+//SELECTION PAGE
 $(document).ready(function () {
-  collapseNavItem();
   toggleSidebar();
-  // drawChartSHU();
-  // drawChartDT();
-  subFormHide();
+  collapseNavItem();
+
+  switch ($("body").data("titlepage")) {
+    case "dashboard":
+      drawChartSHU();
+      drawChartDT.main();
+      refreshChartDT();
+      break;
+    case "tambahanggota":
+      subFormHide();
+      break;
+    case "detailusers":
+      drawChartAktifitasUser();
+      drawChartTransaksiUser();
+      tabMoveUsers();
+      tabMovefromChartLabelUsers();
+      subFormHide();
+      maximizeTable();
+      break;
+    case "tambahsimpanan":
+      drawYear();
+      break;
+    case "tambahpinjaman":
+      countAngsurBulan();
+      break;
+  }
 });
 
 
 
-// Tambah Anggota
-function subFormHide() {
-  subFormShowHideBt.each(function(index) {
-    $(this).click(function(e){
-      e.preventDefault();
-      const subFormShowHideBtArea = $(this).attr("areahid");
-      const sttsSubform = $(this).hasClass("subformCollapse");
-      $(`#${subFormShowHideBtArea}`).slideToggle();
-      if (sttsSubform) {
-        $(this).removeClass("subformCollapse");
-        $(this).addClass("CollapseReverse");
-      } else {
-        $(this).removeClass("CollapseReverse");
-        $(this).addClass("subformCollapse");
-      }
-    });
-  });
-}
+//==================================================================DEFINITION FUNCTION PERPAGE================================================================//
 
 
-
-//Sidebar
+//SIDEBAR
 function collapseNavItem() {
   $('.navitem-collapse').each(function (index) {
     $(this).click(function (e) {
       e.preventDefault();
       var area = $(this).attr('area-collapse');
       var iconCollapse = $(this).attr('btn-collapse');
-      var sttsarea = $(`#${iconCollapse}`).hasClass('sidebarItemCollapse');
+      var sttsarea = $(`#${iconCollapse}`).hasClass('sidebarItemCollapseNot');
       $(`#${area}`).slideToggle();
 
       if (sttsarea) {
         $(this).addClass('navitemCollapsed');
-        $(`#${iconCollapse}`).removeClass('sidebarItemCollapse');
-        $(`#${iconCollapse}`).addClass('CollapseReverse');
+        $(`#${iconCollapse}`).removeClass('sidebarItemCollapseNot');
+        $(`#${iconCollapse}`).addClass('CollapseReverseNot');
       } else {
-        $(`#${iconCollapse}`).addClass('sidebarItemCollapse');
-        $(`#${iconCollapse}`).removeClass('CollapseReverse');
+        $(`#${iconCollapse}`).addClass('sidebarItemCollapseNot');
+        $(`#${iconCollapse}`).removeClass('CollapseReverseNot');
         $(this).removeClass('navitemCollapsed');
       }
     });
@@ -67,10 +62,16 @@ function collapseNavItem() {
 }
 
 function toggleSidebar(){
-  $("#menu-toggle").click(function(e) {
+  $("#menu-toggle").click(function (e) {
     e.preventDefault();
     var itemsmhidden = $(".item-sm-hidden").hasClass("d-none");
-    $(".sidebar").toggleClass("toggled");
+    var paramSidebar;
+    if ($(window).width() < 992) {
+      paramSidebar = "toggledSm";
+    } else {
+      paramSidebar = "toggled";
+    }
+    $(".sidebar").toggleClass(paramSidebar);
     if (itemsmhidden) {
       $(".hiddenlabel").removeClass("d-none");
       $(".navitemLink").removeClass("text-center");
@@ -94,55 +95,181 @@ function toggleSidebar(){
     }
   });
 }
+//END SIDEBAR
 
 
-// Dashboard
-function drawChartSHU() {
-  var ctx = document.getElementById("pertumbuhanSHU");
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-      datasets: [
-        {
-          label: "Population (millions)",
-          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-          data: [2478,5267,734,784,433]
+// DASHBOARD
+function drawChartSHU(e) {
+  var data = {
+    labels: ['2015', '2016', '2017', '2018', '2019'],
+    series: [
+      [500, 400, 300, 700, 500],
+    ]
+  };
+
+  var options = {
+    seriesBarDistance: 12,
+    height: '300px'
+  };
+
+  var responsiveOptions = [
+    ['screen and (max-width: 640px)', {
+      seriesBarDistance: 5,
+      axisX: {
+        labelInterpolationFnc: function (value) {
+          return value[0];
         }
-      ]
-    },
-    options: {
-      responsive:true,
-      legend: { display: false },
-      title: {
-        display: true,
-        text: 'Predicted world population (millions) in 2050'
       }
+    }]
+  ];
+  new Chartist.Bar('#pertumbuhan', data, options, responsiveOptions);
+}
+
+const drawChartDT = {
+  main: function (){
+    var Chart = new Chartist.Pie('#detailtransaksi', {
+      series: [60, 30, 10]
+    }, {
+      height: '260px',
+      donut: true,
+      donutWidth: 20,
+      donutSolid: true,
+      startAngle: 0,
+      showLabel: true
+    });
+    return Chart;
+  }, 
+  update: function () {
+    const data = { series: [20, 30, 50] };
+    drawChartDT.main().update(data);
+  }
+}
+
+function refreshChartDT() {
+  $('#refreshchartDT').click(function (e) {
+    e.preventDefault();
+    drawChartDT.update();
+  });
+}
+//END DASHBOARD
+
+
+// TAMBAH ANGGOTA
+function subFormHide() {
+  $('.showsubform').each(function(index) {
+    $(this).click(function (e) {
+      e.preventDefault();
+      const subFormShowHideBtArea = $(this).attr("areahid");
+      const sttsSubform = $(this).hasClass("subformCollapse");
+      $(`#${subFormShowHideBtArea}`).slideToggle();
+      if (sttsSubform) {
+        $(this).removeClass("subformCollapse");
+        $(this).addClass("CollapseReverse");
+      } else {
+        $(this).removeClass("CollapseReverse");
+        $(this).addClass("subformCollapse");
+      }
+    });
+  });
+}
+//END TAMBAH ANGGOTA
+
+
+//DETAIL USER
+function drawChartAktifitasUser() {
+  new Chartist.Line('#chartAktivitas', {
+    labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei'],
+    series: [
+      [5, 9, 7, 8, 12],
+    ]
+  }, {
+    fullWidth: true,
+    height: "220px",
+    chartPadding: {
+      right: 40
     }
-});
+  });
+}
+
+function drawChartTransaksiUser() {
+  new Chartist.Pie('#chartTransaksiUsers', {
+    series: [60, 30, 10]
+  }, {
+    height: '200px',
+    donut: true,
+    donutWidth: 20,
+    donutSolid: true,
+    startAngle: 0,
+    showLabel: true
+  });
+}
+
+function tabMoveUsers() {
+   $('.navigate-items').each(function(index) {    
+      $(this).click(function (e) {
+          e.preventDefault();
+          $("div.tab-pane").removeClass("active");
+          $("li.navigate-items").removeClass("active");
+          const currentTab = $(this).data("tabdetail");
+          $(`#${currentTab}`).addClass("active");
+          $(this).addClass("active");
+      });
+  });
+}
+
+function tabMovefromChartLabelUsers() {
+  $('.navigate-itemslabel').each(function(index) {    
+      $(this).click(function (e) {
+          e.preventDefault();
+          $("div.tab-pane").removeClass("active");
+          $("li.navigate-items").removeClass("active");
+          const currentTab = $(this).data("tabdetail");
+          $(`#${currentTab}`).addClass("active");
+          $(`.navigate-items[data-tabdetail="${currentTab}"]`).addClass("active");
+      });
+  });
+}
+
+function maximizeTable() {
+  $('#mindetailuser').hide();
+  $('#maximizedetailuser').click(function (e) {
+    e.preventDefault();
+    $('#topdetailuser').hide();
+    $(this).hide();
+    $('#mindetailuser').show();
+    $('.user-info-block').removeClass('px-3'); 
+  });
+  $('#mindetailuser').click(function (e) {
+    e.preventDefault();
+    $('#topdetailuser').show();
+    $(this).hide();
+    $('#maximizedetailuser').show();
+    $('.user-info-block').addClass('px-3');
+  });
+}
+//END DETAIL USER
+
+
+// TAMBAH SIMPANAN
+function drawYear() {
+  var currentTime = new Date();
+  for (let index = -2; index <= 2; index++) {
+    $('#yearsimpanan').append(`<option value="${currentTime.getFullYear() + index }">${currentTime.getFullYear() + index}</option>`);
+  }
 }
 
 
-function drawChartDT() {
-  var ctx = document.getElementById("detailTransaksi");
-  new Chart(ctx, {
-   type: 'doughnut',
-    data: {
-      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-      datasets: [
-        {
-          label: "Population (millions)",
-          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-          data: [2478,5267,734,784,433]
-        }
-      ]
-    },
-    options: {
-      responsive:true,
-      title: {
-        display: true,
-        text: 'Predicted world population (millions) in 2050'
-      }
-    }
+// TAMBAH PINJAMAN
+function countAngsurBulan() {
+  var pinjaman = $('#jumlahpinjaman');
+  var angsuran = $('#jumlahangsur');
+  var perbulan = $('#jumlahangsuran');
+
+  angsuran.change(function (e) {
+    e.preventDefault();
+    var hasil = pinjaman.val() / $(this).val();
+    perbulan.val(hasil);
+    console.log('test');
   });
+
 }
